1d86011afe33a7dbcd9d8ad72ae4198f58b67007
Guilherme <guilherme_fcm@hotmail.com>
2023-04-26 22:04:40 -0300

    Second example

--- Fragmentos/app/src/main/java/com/example/fragmentos/MainActivity.java
+++ Fragmentos/app/src/main/java/com/example/fragmentos/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.fragmentos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.fragmentos.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private FragmentManager fragManager;
+    private FragmentTransaction fragTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fragManager = getSupportFragmentManager();
+
+        binding.ButtonTela1.setOnClickListener(view -> {
+           fragTransaction = fragManager.beginTransaction();
+           fragTransaction.replace(R.id.frame, new Tela1());
+           fragTransaction.commit();
+        });
+
+        binding.ButtonTela2.setOnClickListener(view -> {
+            fragTransaction = fragManager.beginTransaction();
+            fragTransaction.add(R.id.frame, new Tela2());
+            fragTransaction.commit();
+        });
     }
 }
