b4de4ff0e7fe2b61bfb5f3f2eee24dac13fcb0ca
Guilherme <guilherme_fcm@hotmail.com>
2023-05-01 14:52:23 -0300

    Create layout

--- MoviesFragments/app/src/main/java/com/example/moviesfragments/MainActivity.java
+++ MoviesFragments/app/src/main/java/com/example/moviesfragments/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.moviesfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.moviesfragments.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private FragmentManager fragManager;
+    private FragmentTransaction fragTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fragManager = getSupportFragmentManager();
     }
 }
