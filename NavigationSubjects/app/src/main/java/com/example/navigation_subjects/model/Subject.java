9436b28cc20297c88bbd0ce2d16c54fca103f0f4
Guilherme <guilherme_fcm@hotmail.com>
2023-08-28 23:50:45 -0300

    Implements first period

---
+++ NavigationSubjects/app/src/main/java/com/example/navigation_subjects/model/Subject.java
@@ -0,0 +1,32 @@
+package com.example.navigation_subjects.model;
+
+public class Subject {
+    private String name;
+    private String difficulty;
+
+    public Subject(String name, String difficulty) {
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + difficulty;
+    }
+}
--- NavigationSubjects/app/src/main/java/com/example/navigation_subjects/ui/period1/Period1Fragment.java
+++ NavigationSubjects/app/src/main/java/com/example/navigation_subjects/ui/period1/Period1Fragment.java
@@ -4,26 +4,37 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableRow;
+import android.widget.ArrayAdapter;
 
-import com.example.navigation_subjects.databinding.FragmentPeriod1Binding;
+import com.example.navigation_subjects.databinding.FragmentPeriodBinding;
+import com.example.navigation_subjects.model.Subject;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Period1Fragment extends Fragment {
-    private FragmentPeriod1Binding binding;
+    private FragmentPeriodBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentPeriod1Binding.inflate(inflater, container, false);
+        binding = FragmentPeriodBinding.inflate(inflater, container, false);
 
         Period1ViewModel viewModel = new ViewModelProvider(this).get(Period1ViewModel.class);
-        viewModel.getText().observe(getViewLifecycleOwner(), binding.text::setText);
+        viewModel.getText().observe(getViewLifecycleOwner(), this::setSubjects);
 
         return binding.getRoot();
+    }
+
+    private void setSubjects(List<Subject> subjects) {
+        List<String> list = subjects.stream().map(Subject::toString).collect(Collectors.toList());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list);
+        binding.list.setAdapter(adapter);
     }
 }
