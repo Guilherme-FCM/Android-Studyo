eda444daf8d0e9101e0149e25b2ceb39ca9f13f5
Guilherme <guilherme_fcm@hotmail.com>
2023-08-29 21:19:10 -0300

    Config marker

--- Maps/app/src/main/java/com/example/maps/MapsActivity.java
+++ Maps/app/src/main/java/com/example/maps/MapsActivity.java
@@ -8,7 +8,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.maps.databinding.ActivityMapsBinding;
 
@@ -45,7 +47,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        MarkerOptions marker = new MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney")
+            .draggable(true)
+            .snippet("Hello")
+            .rotation(90.F)
+            .icon(
+                BitmapDescriptorFactory.fromResource(R.drawable.bird)
+            );
+        mMap.addMarker(marker);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
